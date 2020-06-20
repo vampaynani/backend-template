b23351a5e9c3e7c9180d83f8dd048db8b84a5f10
Wenceslao Negrete <vampay@digitaldealers.mx>
2020-06-19 20:23:10 -0500

    Audit Fix 06-2020

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
+//const { dbConnect } = require('./db-mongoose');
+const { dbConnect } = require('./db-knex');
 
 const app = express();
 
